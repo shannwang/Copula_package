a04950a76bc583e5824a1fb2443a22d697c207da
Shanshan Wang <74112972+shannwang@users.noreply.github.com>
2021-11-17 20:20:51 +0100

    Update copula_density.py

--- src/copula_package/copula_density.py
+++ src/copula_package/copula_density.py
@@ -41,6 +41,7 @@ def draw_histogram(x,n_bins,label_x):
 
 def draw_joint_distribution(z,label_x,label_y):
     # joint distribution of x and y
+    fig, axes=plt.subplots(figsize=(8,6))
     sns.jointplot(data=z, kind="scatter", x=label_x, y=label_y)
     plt.savefig('joint_distribution.png',dpi=300, transparent=False, format='png', bbox_inches='tight')
     plt.close(fig)
